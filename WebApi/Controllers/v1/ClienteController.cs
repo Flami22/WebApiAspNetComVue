2b34a3f1372848b80abd3a9b090bbbac17232afa
João Henrique Pereira Rodrigues <63281362+Flami22@users.noreply.github.com>
2023-10-20 00:08:55 -0300

    Update ClienteController.cs

--- WebApi/Controllers/v1/ClienteController.cs
+++ WebApi/Controllers/v1/ClienteController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Application.ViewModel;
 using WebApi.Domain.DTOs;
-using WebApi.Domain.Model.EmployeeAggregate;
+using WebApi.Domain.Model.Cliente;
 
 namespace WebApi.Controllers.v1
 {
