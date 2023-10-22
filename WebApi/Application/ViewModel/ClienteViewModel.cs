9ae372cdd8a3587b7c21931a0cd16edaabd707a0
João Henrique Pereira Rodrigues <63281362+Flami22@users.noreply.github.com>
2023-10-21 22:41:11 -0300

    Update ClienteViewModel.cs

--- WebApi/Application/ViewModel/ClienteViewModel.cs
+++ WebApi/Application/ViewModel/ClienteViewModel.cs
@@ -3,8 +3,12 @@
     public class ClienteViewModel
     {
         public string Name { get; set; }
-        public int Email { get; set; }
+        public string Email { get; set; }
+        public string password { get; set; }
+        public string cpf { get; set; }
+        public string Data_nascimento { get; set; }
         public List<EnderecoContato> Enderecos { get; set; }
         public IFormFile Photo { get; set; }
     }
 }
+
