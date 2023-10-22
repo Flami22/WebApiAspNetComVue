8c9f9b1e9e34720f204374513baac4201f4ced0b
João Henrique Pereira Rodrigues <63281362+Flami22@users.noreply.github.com>
2023-10-21 23:58:27 -0300

    Update ClienteInputModel.cs

---
+++ WebApi/Application/InputModel/ClienteInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi.Application.InputModel
+{
+    public class ClienteInputModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string password { get; set; }
+        public string cpf { get; set; }
+        public string Data_nascimento { get; set; }
+        public List<EnderecoContato> Enderecos { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+}
