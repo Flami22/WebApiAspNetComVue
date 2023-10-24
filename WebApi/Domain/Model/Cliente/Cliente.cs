f8306bb4a56d997b444cecc9923b3abe5ac8ecaf
João Henrique Pereira Rodrigues <63281362+Flami22@users.noreply.github.com>
2023-10-24 19:31:01 -0300

    Update Cliente.cs

--- WebApi/Domain/Model/Cliente/Cliente.cs
+++ WebApi/Domain/Model/Cliente/Cliente.cs
@@ -12,16 +12,16 @@ namespace WebApi.Domain.Model.Cliente
         public string email { get; private set; }
         public string password { get; private set;}
         public string? photo { get; private set; }
-        public string cpf {get; private set;}
-        public string data_nascimento {get; private set;}
-        public datetime createdAt { get; private set;}
-        public datetime updatedAt { get; private set;}
+        public string cpf { get; private set; }
+        public string data_nascimento {get; private set; }
+        public datetime createdAt { get; private set; }
+        public datetime updatedAt { get; private set; }
         public bool ativo { get; private set; }
         public List<ClienteEndereco> enderecos { get; private set; }
 
         public Cliente (string name, string email, string password, string photo, string cpf, string data_nascimento)
         {   
-            this.name = name ?? throw new ArgumentNullException(nameof(name))
+            this.name = name ?? throw new ArgumentNullException(nameof(name));
             this.email = email;
             this.password = password;
             this.photo = photo;
@@ -29,8 +29,8 @@ namespace WebApi.Domain.Model.Cliente
             this.data_nascimento = data_nascimento;
             this.createdAt = datetime.now;
             this.updatedAt = datetime.now;
-            this.ativo = true
-            this.enderecos = new List<ClienteEndereco>()
+            this.ativo = true;
+            this.enderecos = new List<ClienteEndereco>();
         }
     }
 }
