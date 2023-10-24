d6a33cf8a80261923e6ceba523e4ee1b613f08c8
João Henrique Pereira Rodrigues <63281362+Flami22@users.noreply.github.com>
2023-10-24 19:13:53 -0300

    Update ClienteEnderecoDTO.cs

---
+++ WebApi/Domain/DTOs/ClienteEnderecoDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi.Domain.DTOs
+{
+    public class ClienteEnderecoDTO
+    {
+        public int Id { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Cep { get; set;}
+        public string? Complemento { get; set; }
+        public string Cpf { get; set;}
+        public int ClienteId {get; set;}
+
+    }
+ }
