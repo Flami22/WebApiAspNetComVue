c4ac47e5da2f6026432ec2b4cd760add9a5116be
João Henrique Pereira Rodrigues <63281362+Flami22@users.noreply.github.com>
2023-10-25 09:58:37 -0300

    Update ClienteDTO.cs

--- WebApi/Domain/DTOs/ClienteDTO.cs
+++ WebApi/Domain/DTOs/ClienteDTO.cs
@@ -7,8 +7,8 @@
         public string Email { get; set; }
         public string Password { get; set;}
         public string? Photo { get; set; }
-        public string Cpf {get; set;}
-        public string Data_nascimento
+        public string Cpf { get; set; }
+        public string Data_nascimento { get; set; }
 
     }
 }
