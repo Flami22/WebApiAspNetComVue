38939335e4cfa6be201890b6e8d8d0786871a9c7
João Henrique Pereira Rodrigues <63281362+Flami22@users.noreply.github.com>
2023-10-24 22:44:45 -0300

    Update ClienteEndereco.cs

--- WebApi/Domain/Model/ClienteEndereco/ClienteEndereco.cs
+++ WebApi/Domain/Model/ClienteEndereco/ClienteEndereco.cs
@@ -8,11 +8,22 @@ namespace WebApi.Domain.Model.ClienteEndereco
     public class ClienteEndereco
     {
         [Key]
-        public int Id { get; set; }
-        public string Logradouro { get; set; }
-        public string Numero { get; set; }
-        public string Cep { get; set; }
-        public string Complemento { get; set; }
-        public int ClienteId { get; set; }
+        public int Id { get; private set; }
+        public string Logradouro { get; private set; }
+        public string Numero { get; private set; }
+        public string Cep { get; private set; }
+        public string Complemento { get; private set; }
+        public int ClienteId { get; private set; }
+
+        public void ClienteEndereco (string logradoro, string numero, string cep, string complemento, int clienteId)
+        {
+            this.Logradouro = logradouro;
+            this.Numero = numero;
+            this.Cep = cep;
+            this.Complemento = complemento;
+            this.ClienteId = clienteId;
+                
+        }
+
     }
 }
