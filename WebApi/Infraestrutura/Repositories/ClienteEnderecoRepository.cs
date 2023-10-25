c066239b561a0d1c6fe814c8eb10cfdab622065d
João Henrique Pereira Rodrigues <63281362+Flami22@users.noreply.github.com>
2023-10-25 09:25:47 -0300

    Update ClienteEnderecoRepository.cs

---
+++ WebApi/Infraestrutura/Repositories/ClienteEnderecoRepository.cs
@@ -0,0 +1,25 @@
+using WebApi.Domain.Model.Cliente;
+
+namespace WebApi.Infraestrutura.Repositories.ClienteRepository 
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly ConnectionContext _context = new ConnectionContext();
+
+        public List<ClienteEnderecoDTO> Get(int id)
+        {
+            return _context.ClienteEndereco.ToList();
+        }
+        
+         public void AtualizarEndereco (ClienteEndereco clienteEndereco)
+         {
+             
+         }
+        
+        public void AddEndereco (ClienteEndereco clienteEndereco)
+        {
+            _context.ClienteEndereco.add(clienteEndereco);
+            _context.SaveChanges();
+        }
+    }
+}
