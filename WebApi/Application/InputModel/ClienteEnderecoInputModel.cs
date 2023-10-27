7bfc2d6da3385e0f275827ae0592d2f2137c86e6
João Henrique Pereira Rodrigues <63281362+Flami22@users.noreply.github.com>
2023-10-27 20:52:33 -0300

    Update ClienteEnderecoInputModel.cs

---
+++ WebApi/Application/InputModel/ClienteEnderecoInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi.Application.InputModel
+{
+    public class ClienteEndereco
+    {
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Cep { get; set;}
+        public string? Complemento { get; set; }
+        public string Cpf { get; set;}
+        public int ClienteId {get; set;}
+
+    }
+ }
